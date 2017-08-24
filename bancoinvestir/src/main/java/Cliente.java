726c62024014778a5f15450afb74506ea39fd42d
raquel <quequel.barcelos6@gmail.com>
2017-08-24 16:07:00 -0300

    salvos

---
+++ bancoinvestir/src/main/java/Cliente.java
@@ -0,0 +1,81 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the rg
+     */
+    public int getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public int getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the dtnasc
+     */
+    public Date getDtnasc() {
+        return dtnasc;
+    }
+
+    /**
+     * @param dtnasc the dtnasc to set
+     */
+    public void setDtnasc(Date dtnasc) {
+        this.dtnasc = dtnasc;
+    }
+    
+
+
+public Cliente() { }
+
+    private String nome;
+    private int rg;
+    private int cpf;
+    private Date dtnasc;
+
+} 
---
+++ bancoinvestir/src/main/java/Conta.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Conta {
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the agencia
+     */
+    public int getAgencia() {
+        return agencia;
+    }
+
+    /**
+     * @param agencia the agencia to set
+     */
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public float getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the limite
+     */
+    public float getLimite() {
+        return limite;
+    }
+
+    /**
+     * @param limite the limite to set
+     */
+    public void setLimite(float limite) {
+        this.limite = limite;
+    }
+    
+    public Conta() {}
+        
+    private int numero;
+    private int agencia;
+    private float saldo; 
+    private float limite;
+    
+        
+       
+    
+}
